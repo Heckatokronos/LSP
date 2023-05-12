52fab52857f20f7ee225fff80c147effc51453d3
Alexander Abramov <snlyshigoto@gmail.com>
2023-05-12 22:05:46 +0700

    added category controller funcs

--- server/app/controllers/api/v1/categories_controller.rb
+++ server/app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::CategoriesController < ApplicationController
+  def index
+    render json: Category.all
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      render json: @category, status: :created
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    
+    @category.destroy
+    render json: @category, status: :see_other
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
