dd57221569d2802da94acd0e95b5cd85353b67c5
Alexander Abramov <snlyshigoto@gmail.com>
2023-05-12 21:21:56 +0700

    added validation for categories

--- server/app/models/category.rb
+++ server/app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
+  validates :name, presence: true,
+                   uniqueness: true,
+                   format: { with: /\A[a-zA-Z]+\z/, message: "Allows only letters" },
+                   length: { in: 4..20 }
   has_many :posts
 end
